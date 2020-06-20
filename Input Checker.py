145ff00630ce81a24522763f704dc105b72e1796
gaurimangire <65966022+gaurimangire@users.noreply.github.com>
2020-06-20 19:53:29 +0530

    Update Input Checker.py

--- Input Checker.py
+++ Input Checker.py
@@ -1,13 +1,13 @@
 input1 = input("Enter anything you want:-")
 input2 = input("Enter anything you want:-")
 
-if input1.isdigit():
-    if input2.isdigit():
-        x = int(input1) + int(input2)
-        print(x)
+if input1.isdigit() and input2.isdigit():
+    x = int(input1) + int(input2)
+    print(x)
     
-elif input1.isalpha():
-    if input2.isalpha():
-        print(input1+input2)
+elif input1.isalpha() and input2.isalpha():
+    print(input1+input2)
+
 else:
     print("It is something different")
+
