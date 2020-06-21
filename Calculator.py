5a7aa9442dc75dd51ca1ad26767aa3a6aa867fc8
gaurimangire <65966022+gaurimangire@users.noreply.github.com>
2020-06-21 15:53:34 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -18,33 +18,33 @@ while option != 7:
 6. To the Power Values(In this the 1st number is base value and the 2nd is index value)
 7. Quit
 """)
-    option = int(input("Enter the option from the above calculation:-"))
+    option = int(input("Enter the option from the above operation:-"))
     if option == 7:
         break
     while x != "No" or x != "no":
         num1 = int(input("Enter a number:-"))
         num2 = int(input("Enter another number:-"))
         if option == 1:
-            print("The addition of "+str(num1) + "and " +str(num2) + " is:-" +str(num1+num2))
+            print("The addition of "+str(num1) + " and " +str(num2) + " is:-" +str(num1+num2))
         elif option == 2:
             if num1 >= num2:
-                print("The subtraction of "+str(num1) + "and " +str(num2) + " is:-" +str(num1-num2))
+                print("The subtraction of "+str(num1) + " and " +str(num2) + " is:-" +str(num1-num2))
             else:
-                print("The subtraction of "+str(num1) + "and " +str(num2) + " is:-" +str(num2-num1))
+                print("The subtraction of "+str(num1) + " and " +str(num2) + " is:-" +str(num2-num1))
         elif option == 3:
-            print("The multiplication of "+str(num1) + "and " +str(num2) + " is:-" +str(num1*num2))
+            print("The multiplication of "+str(num1) + " and " +str(num2) + " is:-" +str(num1*num2))
         elif option == 4:
             if num1 >= num2:
-                print("The division of "+str(num1) + "and " +str(num2) + " is:-" +str(num1/num2))   
+                print("The division of "+str(num1) + " and " +str(num2) + " is:-" +str(num1/num2))   
             else:
-                print("The division of "+str(num1) + "and " +str(num2) + " is:-" +str(num2/num1))
+                print("The division of "+str(num1) + " and " +str(num2) + " is:-" +str(num2/num1))
         elif option == 5:
             if num1 >= num2:
-                print("The remainder of "+str(num1) + "and " +str(num2) + " is:-" +str(num1%num2))
+                print("The remainder of "+str(num1) + " and " +str(num2) + " is:-" +str(num1%num2))
             else:
-                print("The remainder of "+str(num1) + "and " +str(num2) + " is:-" +str(num2%num1))
+                print("The remainder of "+str(num1) + " and " +str(num2) + " is:-" +str(num2%num1))
         elif option == 6:
-            print(str(num1) + " raise to " +str(num2) + "is:-"+str(num1**num2))
+            print(str(num1) + " raise to " +str(num2) + " is:-"+str(num1**num2))
         
         x = input("Do you wish to continue:-")
         if x == "Yes" or x == "yes":
